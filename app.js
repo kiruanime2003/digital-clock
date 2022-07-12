879a8928ace4769a78e0c3a02f0d2a05099411a9
Kiruthiga Kanagalingam <kiruanime2003@gmail.com>
2022-07-12 19:40:16 +0530

    added sunrise and sunset pics

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
-    
+
     function getTime(){
         var displayHours = document.getElementById("hours");
         var displayMinutes = document.getElementById("minutes");
         var displaySeconds = document.getElementById("seconds");
+        var leftColon = document.getElementById("left-colon");
+        var rightColon = document.getElementById("right-colon");
 
         var fetchTime = new Date;
         var fetchHours = fetchTime.getHours();
@@ -17,6 +19,23 @@ document.addEventListener("DOMContentLoaded", function(){
         displayHours.innerHTML = fetchHours;
         displayMinutes.innerHTML = fetchMinutes;
         displaySeconds.innerHTML = fetchSeconds;
+
+        if(fetchHours<=12){
+            document.body.style.backgroundImage = "url('Images/sunrise.jpg')";
+            displayHours.style.color = "#0D1B1E";
+            displayMinutes.style.color = "#0D1B1E";
+            displaySeconds.style.color = "#0D1B1E";
+            leftColon.style.color = "#0D1B1E";
+            rightColon.style.color = "#0D1B1E";
+        }
+        else{
+            document.body.style.backgroundImage = "url('Images/sunset.jpg')";
+            displayHours.style.color = "#EBEBEB";
+            displayMinutes.style.color = "#EBEBEB";
+            displaySeconds.style.color = "#EBEBEB";
+            leftColon.style.color = "#EBEBEB";
+            rightColon.style.color = "#EBEBEB";
+        }
     };
 
     setInterval(getTime, 1000); //execute the code every 1 second
